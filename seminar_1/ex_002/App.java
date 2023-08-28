6f4cb9e2980a74e0c5fc8b3d8938739cde9f2289
Deskardo <kalugin.aleksei.v@gmail.com>
2023-08-28 20:41:32 +0300

    example 2 complete

--- seminar_1/ex_002/App.java
+++ seminar_1/ex_002/App.java
@@ -2,6 +2,15 @@ package seminar_1.ex_002;
 
 public class App {
     public static void main(String[] args) {
-        
+        // создание объектов разных классов
+        Mammal lion = new Mammal("Lion", 5, "Yellow");
+        Bird sparrow = new Bird("Sparrow", 2, true);
+
+        // Использование методов и вывод информации
+        lion.makeSound();
+        lion.displayInfo();
+
+        sparrow.makeSound();
+        sparrow.displayInfo();
     }
 }
