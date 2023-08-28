91a550dbc336826b95ffe08e4d9678753e8225f8
Deskardo <kalugin.aleksei.v@gmail.com>
2023-08-28 07:27:34 +0300

    seminar 1_create class Animal

---
+++ seminar_1/ex_001/Animal.java
@@ -0,0 +1,23 @@
+package seminar_1.ex_001;
+
+class Animal {
+    String name;
+    int age;
+
+    // конструктор класса Animal
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // метод для издания звука
+    public void makeSoung() {
+        System.out.println("The animal makes a sound.");
+    }
+
+    // метод для вывода информации о животном
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
