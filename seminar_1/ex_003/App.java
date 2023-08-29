0ae1f5a608d3f9fae03fb1a7a3de51145b6c9266
Deskardo <kalugin.aleksei.v@gmail.com>
2023-08-29 17:58:53 +0300

    class Dog complete, seminar_1 complete

--- seminar_1/ex_003/App.java
+++ seminar_1/ex_003/App.java
@@ -2,6 +2,23 @@ package seminar_1.ex_003;
 
 public class App {
     public static void main(String[] args) {
-   
+        // создание объекта собаки
+        Dog labrador = new Dog("Buddy", 3, "Labrador");
+
+        // использование геттеров и сеттеров
+        System.out.println("Name: " + labrador.getName());
+        System.out.println("Age: " + labrador.getAge());
+
+        // обновление данных собаки
+        labrador.setName("Max");
+        labrador.steAge(4);
+
+        // вывод обновленных данных
+        System.out.println("Updated Name: "+ labrador.getName());
+        System.out.println("Updated Age: " + labrador.getAge());
+
+        // вызов методов и вывод информации о собаке
+        labrador.makeSound();
+        labrador.displayInfo();
     }
 }
