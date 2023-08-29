8cdadd54024c92d52651425ef85a694b42390e52
Deskardo <kalugin.aleksei.v@gmail.com>
2023-08-29 21:38:23 +0300

    hw 1 example 3 complete

--- home_work_1/ex_003/App.java
+++ home_work_1/ex_003/App.java
@@ -1,10 +1,40 @@
 package home_work_1.ex_003;
 
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        Book book1 = new Book("Этюды черни", "Ольга Усакова", true);
+        ArrayList<Book> catalog = new ArrayList<Book>();
+        Library library = new Library(catalog);
 
-        book1.displayInfo();
+        Book book = new Book("Этюды черни", "Ольга Ускова", true);
+        Book book1 = new Book("Благословение небожителей", "Тунсю Мосян", true);
+        Book book2 = new Book("Оды", "Квинт Гораций", false);
+        Book book3 = new Book("Ловушка для адвоката", "Майкл Коннелли", true);
+        Book book4 = new Book("Пуля для адвоката", "Майкл Коннелли", true);
+        Book book5 = new Book("Переступить черту", "Майкл Коннелли", true);
+        Book book6 = new Book("Дома стоят дольше, чем люди", "Виктория Токарева", true);
+
+        library.addBook(book);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+        library.addBook(book6);
+
+        library.displayAvailableBooks();
+
+        System.out.println("");
+
+        library.removeBook(book6);
+
+        library.displayAvailableBooks();
+        System.out.println("");
+
+        library.searchByAuthor("Майкл Коннелли");
+        
+
     }
-    
+
 }
--- home_work_1/ex_003/Library.java
+++ home_work_1/ex_003/Library.java
@@ -1,5 +1,43 @@
 package home_work_1.ex_003;
 
-public class Library {
-    
+import java.util.*;
+
+class Library {
+    private ArrayList<Book> catalog = new ArrayList<>();
+
+    // базовый конструктор каталога
+    public Library(ArrayList<Book> catalog) {
+        this.catalog = catalog;
+    }
+
+    // добавление книг в каталог
+    public void addBook(Book book) {
+        catalog.add(book);
+    }
+
+    // удаление книг из каталога
+    public void removeBook(Book book) {
+        catalog.remove(book);
+    }
+
+    // цикл для отображения книг
+    public void displayAvailableBooks() {
+        System.out.println("На текущий момент доступны: ");
+        for (Book book : catalog) {
+            if (book.getAvailable()) {
+                book.displayInfo();
+            }
+        }
+    }
+
+    // поиск книги по автору
+    public void searchByAuthor(String author) {
+        System.out.println("По автору " + author + " есть следующие книги: ");
+        for (Book book : catalog) {
+            if (book.getAuthor().equals(author)) {
+                book.displayInfo();
+            }
+        }
+    }
+
 }
