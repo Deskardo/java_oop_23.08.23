981403c4cc3c42f0506a4eae031825704a4fa349
Deskardo <kalugin.aleksei.v@gmail.com>
2023-09-02 14:29:47 +0300

    s2_ex5 fin

--- seminar_2/ex_005/App.java
+++ seminar_2/ex_005/App.java
@@ -2,6 +2,10 @@ package seminar_2.ex_005;
 
 public class App {
     public static void main(String[] args) {
-        
+        // int number = 5;
+
+        int squaredNumber = MathHelper.square(5);
+
+        System.out.println("Square of " + 5 + " is: " + squaredNumber);
     }
 }
---
+++ seminar_2/ex_005/MathHelper.java
@@ -0,0 +1,7 @@
+package seminar_2.ex_005;
+
+interface MathHelper {
+    static int square(int num) {
+        return num * num;
+    }
+}
