b3f9bc8ec6bdd3ce2a033e7d0feacd891bee8f18
Deskardo <kalugin.aleksei.v@gmail.com>
2023-09-02 16:42:52 +0300

    hw2 create IgetSet interface, for class Cat

--- home_work_2/ex_001/Cat.java
+++ home_work_2/ex_001/Cat.java
@@ -1,6 +1,6 @@
 package home_work_2.ex_001;
 
-class Cat extends Owner{
+class Cat extends Owner implements IgetSet{
     private String nickName;
     private int age;
 
@@ -12,21 +12,25 @@ class Cat extends Owner{
     }
 
         // геттер для получения имени кота
+        @Override
         public String getName(){
             return name;
         }
     
         // Сеттер для установки имени кота
+        @Override
         public void setName(String nickName) {
             this.nickName = nickName;
         }
     
         // геттер для получения возраста кота
+        @Override
         public int getAge() {
             return age;
         }
     
         // сеттер для установки возраста кота
+        @Override
         public void setAge(int age) {
             if (age > 0) {
                 this.age = age;
---
+++ home_work_2/ex_001/IgetSet.java
@@ -0,0 +1,12 @@
+package home_work_2.ex_001;
+
+interface IgetSet {
+
+    String getName();
+    int getAge();
+
+    void setName(String nickName);
+    void setAge(int age);
+
+
+}
