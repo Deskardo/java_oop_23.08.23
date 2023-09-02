69672b9dffaf19d8191f803e9dc1074da3e68dd1
Deskardo <kalugin.aleksei.v@gmail.com>
2023-09-02 10:32:13 +0300

    create new method Book

--- home_work_1/ex_003/App.java
+++ home_work_1/ex_003/App.java
@@ -1,39 +1,36 @@
 package home_work_1.ex_003;
 
-import java.util.*;
-
 public class App {
     public static void main(String[] args) {
         Library library = new Library();
 
-        Book book = new Book("Этюды черни", "Ольга Ускова", true);
-        Book book1 = new Book("Благословение небожителей", "Тунсю Мосян", true);
-        Book book2 = new Book("Оды", "Квинт Гораций", false);
-        Book book3 = new Book("Ловушка для адвоката", "Майкл Коннелли", true);
-        Book book4 = new Book("Пуля для адвоката", "Майкл Коннелли", true);
-        Book book5 = new Book("Переступить черту", "Майкл Коннелли", true);
-        Book book6 = new Book("Дома стоят дольше, чем люди", "Виктория Токарева", true);
+        // Book book = new Book("Этюды черни", "Ольга Ускова", true);
+        // Book book1 = new Book("Благословение небожителей", "Тунсю Мосян", true);
+        // Book book2 = new Book("Оды", "Квинт Гораций", false);
+        // Book book3 = new Book("Ловушка для адвоката", "Майкл Коннелли", true);
+        // Book book4 = new Book("Пуля для адвоката", "Майкл Коннелли", true);
+        // Book book5 = new Book("Переступить черту", "Майкл Коннелли", true);
+        // Book book6 = new Book("Дома стоят дольше, чем люди", "Виктория Токарева", true);
 
-        library.addBook(book);
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-        library.addBook(book4);
-        library.addBook(book5);
-        library.addBook(book6);
 
-        library.displayAvailableBooks();
+        // library.addBook(book);
+        // library.addBook(book1);
+        // library.addBook(book2);
+        // library.addBook(book3);
+        // library.addBook(book4);
+        // library.addBook(book5);
+        // library.addBook(book6);
 
-        System.out.println("");
+        // library.displayAvailableBooks();
 
-        library.removeBook(book6);
+        // System.out.println("");
 
-        library.displayAvailableBooks();
-        System.out.println("");
+        // library.removeBook(book6);
 
-        library.searchByAuthor("Майкл Коннелли");
-        
+        // library.displayAvailableBooks();
+        // System.out.println("");
 
+        // library.searchByAuthor("Майкл Коннелли");  
     }
 
 }
--- home_work_1/ex_003/Book.java
+++ home_work_1/ex_003/Book.java
@@ -1,5 +1,7 @@
 package home_work_1.ex_003;
 
+import java.util.Scanner;
+
 public class Book {
     private String title;
     private String author;
@@ -10,6 +12,52 @@ public class Book {
         this.title = title;
         this.author = author;
         this.available = available;
+    }
+
+    public Book() {
+        Scanner scan = new Scanner(System.in);
+        String line = "";
+        Boolean flag = true;
+
+        while (flag) {
+            System.out.println("Введите название книги: ");
+            line = scan.nextLine();
+            if (!line.equals(null) && line.length() > 0) {
+                this.title = line;
+                flag = false;
+            } else {
+                System.out.print("Введено не корректное значение.");
+            }
+        }
+
+        flag = true;
+
+        while (flag) {
+            System.out.println("Введите автора книги: ");
+            line = scan.nextLine();
+            if (!line.equals(null) && line.length() > 0) {
+                this.author = line;
+                flag = false;
+            } else {
+                System.out.print("Введено не корректное значение.");
+            }
+        }
+
+        flag = true;
+
+        while (flag) {
+            System.out.println("Введите показатель доступности книги (true/false): ");
+            line = scan.nextLine().toLowerCase().trim();
+            if (line.equals("true") || line.equals("false")){
+            boolean flag2 = Boolean.parseBoolean(line);
+            this.available = flag2;
+            flag = false;
+            } else {
+                System.out.print("Введено не корректное значение.");
+            }
+        }
+
+        scan.close();
     }
 
     // гетер названия
@@ -49,7 +97,7 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book: title " + title + ", author " + author + ", available " +available;
+        return "Book: title " + title + ", author " + author + ", available " + available;
     }
 
 }
